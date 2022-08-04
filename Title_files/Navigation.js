e54ac575b0ea0fd4047fb2469abee921478d3130
OvercodedStack <vesperiumcreator@gmail.com>
2022-08-04 16:11:04 -0400

    Pushing website..

--- Title_files/Navigation.js
+++ Title_files/Navigation.js
@@ -2,23 +2,141 @@ function reload_funt() {
     location.reload();
  }
 
- function move_to_gallery(){
+ // Movement functions
+ function move_to_projects(){
     window.location.pathname = "Gallery.html";
   }
  function move_to_game(){
     window.location.pathname = "game.html";
   }
  function move_to_index(){
-    window.location.pathname = "";
+    window.location.pathname = "index.html";
   }
- function move_to_Aboutus(){
+ function move_to_downloads(){
     window.location.pathname = "Aboutus.html";
   }
-function move_to_contactUs(){
-    window.location.pathname = "Contactus.html";
 
+function move_to_contact_us(){
+    window.location.pathname = "Contactus.html";
+}
+
+
+
+//Testing for reusable code.
+
+
+const headerTemplate = document.createElement('template');
+headerTemplate.innerHTML = `
+
+<!--<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">-->
+<!--<link rel="stylesheet" type="text/css" href="general_style.css">-->
+
+<div class="header" >
+<!--        <h1> Header - My First Blog on Web Component </h1>-->
+<!-- Buttons -->
+
+    <aside>
+
+        <div class="top_bottom_box"></div>
+
+
+    </aside>
+
+    <!--Main logo-->
+    <div class="mySlides fade">
+        <div style="padding-left:17px">
+<!--        <img src="Images/Logo_Mk2_1.png" style="width:500px; height:200px;">-->
+            <img src="Images/Legacy Website Assets/New_Logo.png" style="width:825px; height:130px;">
+        </div>
+    </div>
+
+
+    <div class="centering_property">
+        <div class="mysquare"></div>
+        <button class="btn_2" onclick="move_to_index()">Home</button>
+<!--        <button class="btn_2" onclick="move_to_projects()">Projects</button>-->
+        <button class="btn_2" onclick="move_to_contact_us()">Contact Details</button>
+<!--        <button class="btn_2" onclick="move_to_downloads()">Downloads</button>-->
+<!--        <button class="btn_2" onclick="move_to_game()">Random Games</button>-->
+        <div class="mysquare"></div>
+    </div>
+</div>
+`
+
+class Header extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
+    }
+    connectedCallback() {
+        while (this.childNodes.length > 0) {
+            this.shadowRoot.appendChild(this.childNodes[0]);
+        }
+
+        const cssFiles = this.getAttribute('css-files');
+
+        if (cssFiles) {
+            const arrCssFiles = cssFiles.split(',');
+
+            // for(let i = 0; i < arrCssFiles.length; i++){
+            let link = document.createElement('link');
+            link.setAttribute('rel', 'stylesheet');
+            link.setAttribute('href', arrCssFiles[0]);
+            const shadowRoot = this.attachShadow({ mode: 'open' });
+            shadowRoot.appendChild(headerTemplate.content);
+            shadowRoot.appendChild(link);
+            // }
+        }
+    }
+
+    // connectedCallback() {
+    //     const shadowRoot = this.attachShadow({ mode: 'open' });
+    //     shadowRoot.appendChild(headerTemplate.content);
+    // }
+}
+
+customElements.define('header-component', Header);
+
+
+
+const footerTemplate = document.createElement('template');
+footerTemplate.innerHTML = `
+<div>
+<div class="rail"></div>-->
+<footer style="text-align:center">
+  <h5> Copyright 2022 - Esteban Segarra -<a href="https://kovant.wordpress.com">Kovant</a> </h5>
+</footer>
+</div>
+`
+class Footer extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
+    }
+    connectedCallback() {
+        while (this.childNodes.length > 0) {
+            this.shadowRoot.appendChild(this.childNodes[0]);
+        }
+
+        const cssFiles = this.getAttribute('css-files');
+
+        if (cssFiles) {
+            const arrCssFiles = cssFiles.split(',');
+            let link = document.createElement('link');
+            link.setAttribute('rel', 'stylesheet');
+            link.setAttribute('href', arrCssFiles[0]);
+            const shadowRoot = this.attachShadow({ mode: 'open' });
+            shadowRoot.appendChild(footerTemplate.content);
+            shadowRoot.appendChild(link);
+        }
+    }
 
 }
+customElements.define('footer-component', Footer);
+
+
+
+
 
 
 
--- Title_files/moving_train.js
+++ Title_files/moving_train.js
@@ -2,50 +2,97 @@
         var imgObj = null;
         var animate ;
         var stopped = false;
-        var target_stop = 600;
+        var target_stop = 500;
+        var train_cars = [];
+        var slow_speed = 4
+        var respawn_location = 5 * 400
 
         //Initialization and cloning of child objects
         function init() {
-           imgObj = document.getElementById('myImage');
-           imgObj.style.position= 'relative';
-           imgObj.style.left = '0px';
-           imgObj2 = document.getElementById('myImage2');
-           imgObj2.style.position= 'relative';
-           imgObj2.style.left = '0px';
-           moveRight();
+            imgObj = document.getElementById('myImage');
+            imgObj.style.position= 'relative';
+            imgObj.style.left = '0px';
+
+
+            car1 = document.getElementById('trainCar1');
+            car2 = document.getElementById('trainCar2');
+            car3 = document.getElementById('trainCar3');
+            // car4 = document.getElementById('trainCar4');
+
+
+            car1.style.position = 'relative';
+            car2.style.position = 'relative';
+            car3.style.position = 'relative';
+            // car4.style.position = 'relative';
+
+
+            car1.style.left = '0px';
+            car2.style.left = '0px';
+            car3.style.left = '0px';
+
+            imgObj2 = document.getElementById('myImage2');
+            imgObj2.style.position= 'relative';
+            imgObj2.style.left = '0px'
+
+            moveRight();
         }
         //Actual function that moves the objects
         function moveRight() {
-            //Get my train position
+
+            //Get my train position;
+            //Returns an object of the rectangle representing the image of the train.
             var rect = imgObj.getBoundingClientRect();
 
-           //"Real" movement
+           //Set the stopping location as the location of the set destination with the right side of the image of the
+            // first train engine.
            var stop_dist = target_stop - rect.right;
+           //Get the floor of the decimal location.
            stop_dist = Math.abs(stop_dist);
 
-           if (stop_dist < 400){
-               var vel = stop_dist/100;
+
+
+           if (stop_dist < 350){
+               //Change the speed depending on the proximaty to the end location
+               var vel = stop_dist/50;
                if (vel < 1.5){
                     vel = 1.5;
                }
-               console.log(vel);
+               if (vel > 4){
+                   vel = 4.0;
+               }
+
+               // console.log(vel);
                imgObj.style.left = parseInt(imgObj.style.left) + vel + 'px';
                imgObj2.style.left = parseInt(imgObj2.style.left) + vel + 'px';
-           }else{
+               // train_cars.forEach(move_cars(imgObj.style.left))
 
-           imgObj.style.left = parseInt(imgObj.style.left) + 4 + 'px';
-           imgObj2.style.left = parseInt(imgObj2.style.left) + 4 + 'px';
+                car1.style.left = parseInt(imgObj2.style.left) + vel + 'px';
+               car2.style.left = parseInt(imgObj2.style.left) + vel + 'px';
+               car3.style.left = parseInt(imgObj2.style.left) + vel + 'px';
+
+
+           }else{
+               imgObj.style.left = parseInt(imgObj.style.left) + slow_speed + 'px';
+               imgObj2.style.left = parseInt(imgObj2.style.left) + slow_speed + 'px';
+
+
+               car1.style.left = parseInt(imgObj2.style.left) + slow_speed + 'px';
+               car2.style.left = parseInt(imgObj2.style.left) + slow_speed + 'px';
+               car3.style.left = parseInt(imgObj2.style.left) + slow_speed + 'px';
            }
 
 
            //If we get off the screen, jump back to the main screen
            if (rect.right > 1920){
                 //This is really ugly
+               //For some godforsaken reason this ACTUALLY MOVES ALL THE CARS
                 stopped = false;
-                imgObj.style.left = parseInt(imgObj.style.left) + -2400 + 'px';
-                imgObj2.style.left = parseInt(imgObj2.style.left) + -2400 + 'px';
+                imgObj.style.left = parseInt(imgObj.style.left) + -3400 + 'px';
+                imgObj2.style.left = parseInt(imgObj2.style.left) + -3400 + 'px';
            }
 
+
+           //Stops the animation
            if (rect.right > target_stop && rect.right < target_stop + 10 && stopped == false){
                animate = setTimeout(moveRight,5000);
                stopped = true;
@@ -55,4 +102,12 @@
                 animate = setTimeout(moveRight,20);    // call moveRight in 20msec
            }
         }
+
+        // move_cars(value,dist)
+        // {
+        //
+        // }
+
+
+
 		window.onload = init;
